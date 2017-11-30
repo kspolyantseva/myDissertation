5df57f86e2a5b862a6c2510c2cb138efba401765
kspolyantseva <ks.polyantseva@yandex.ru>
2017-12-01 02:50:19 +0300

    add StatFile

--- diss/StatViewController.swift
+++ diss/StatViewController.swift
@@ -11,13 +11,28 @@ import Alamofire
 import SwiftyJSON
 
 class StatViewController: UIViewController {
-
+    
+    let URL_GET_USERS = "http://62.109.0.179:3000/users";
+    let URL_USER_TRACKS = "http://62.109.0.179:3000/trackUser?username=" // добавить имя пользователя !!!!!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // запрос юзеров
+        Alamofire.request(URL_GET_USERS, method: .get, parameters: [:]).responseJSON
+            {
+                response in
+                //printing response
+                print(response)
+        }
+        
+        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-
+    
 }
